d760a66be5c8cc24be71d9bd1d3ef967d7b6a7e9
chlchoo <86097790+chlchoo@users.noreply.github.com>
2022-01-24 16:16:30 +0900

    메신저 주고 받기

--- src/public/js/app.js
+++ src/public/js/app.js
@@ -8,11 +8,21 @@ room.hidden = true;
 
 let roomName;
 
-function addMessage (message){
+function addMessage(message) {
   const ul = room.querySelector("ul");
   const li = document.createElement("li");
   li.innerText = message;
   ul.appendChild(li);
+}
+
+function handleMessageSubmit(event) {
+  event.preventDefault();
+  const input = room.querySelector("input");
+  const value = input.value;
+  socket.emit("new_message", input.value, roomName, () => {
+    addMessage(`You: ${value}`);
+  });
+  input.value = "";
 }
 
 function showRoom() {
@@ -20,6 +30,8 @@ function showRoom() {
   room.hidden = false;
   const h3 = room.querySelector("h3");
   h3.innerText = `Room ${roomName}`;
+  const form = room.querySelector("form");
+  form.addEventListener("submit", handleMessageSubmit);
 }
 
 function handleRoomSubmit(event){
@@ -28,11 +40,16 @@ function handleRoomSubmit(event){
     socket.emit("enter_room", input.value, showRoom);
     roomName = input.value;
     input.value = "";
-
 }
 
 form.addEventListener("submit", handleRoomSubmit);
 
 socket.on("welcome", () => {
-  addMessage("someone joined");
-})
+  addMessage("someone joined!");
+});
+
+socket.on("bye", () => {
+  addMessage("someone left ㅠㅠ");
+});
+
+socket.on("new_message", addMessage);
